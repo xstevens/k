2e6b3966eaf9599fc3780d3bc2cb5ddc5481a7c3
Xavier Stevens <xavier@simple.com>
2016-10-25 15:03:25 -0700

    Adding environment variables to usage and updating README

--- README.md
+++ README.md
@@ -44,29 +44,48 @@ GOOS=linux GOARCH=amd64 go build
 
 ## Usage
 ```
-$ ./k help
-Usage: k <command> [options] [arguments]
+$ ./k
+usage: k [<flags>] <command> [<args> ...]
 
-Environment Variables:
+A general command line client for Apache Kafka.
+
+Environment variables:
     KAFKA_BROKERS or TLS_KAFKA_BROKERS
     SSL_CA_BUNDLE_PATH
     SSL_CRT_PATH
     SSL_KEY_PATH
 
-Commands:
-    produce     produce messages to given topic
-    consume     consume messages from given topic
-    consumers    list all consumer groups
-    offsets     show the oldest and newest offset for a given topic and partition
-    topics      show the list of topics
-    tls         connect to broker using Transport Layer Security
-    help        show help
+Optional flags:
+  -h, --help     Show context-sensitive help (also try --help-long and --help-man).
+      --version  Show application version.
 
-Run 'k help <command>' for details.
+Commands:
+  help [<command>...]
+    Show help.
+
+  consume --topic=TOPIC [<flags>]
+    Consumes messages from the given topic and writes them to stdout.
+
+  consumers [<flags>]
+    Gets a list of all consumer groups from brokers. (0.9+ compatible only)
+
+  offsets --topic=TOPIC [<flags>]
+    Prints oldest and newest offsets for the given topic to stdout.
+
+  produce --topic=TOPIC [<flags>]
+    Produces a message to the given topic with the data given by reading stdin (newline delimited).
+
+    If the message includes a tab character, the content before the tab will be interpreted as the message's key.
+
+  tls
+    Connects to broker using Transport Layer Security which can be useful for TLS handshake debugging.
+
+  topics [<flags>]
+    Prints the list of topics to stdout.
 ```
 
 ## License
 All aspects of this software are distributed under the MIT License. See LICENSE file for full license text.
 
 ## Inspirations
-Heavily inspired (grabbed basis of code from) [Mark McGranaghan's zk CLI for Zookeeper](https://github.com/mmcgrana/zk).
+Originally inspired by [Mark McGranaghan's zk CLI for Zookeeper](https://github.com/mmcgrana/zk).
--- main.go
+++ main.go
@@ -8,16 +8,76 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
+// based off of kingpin.SeparateOptionalFlagsUsageTemplate but added
+// a section on environment variables
+var FlagsUsageTemplate = `{{define "FormatCommand"}}\
+{{if .FlagSummary}} {{.FlagSummary}}{{end}}\
+{{range .Args}} {{if not .Required}}[{{end}}<{{.Name}}>{{if .Value|IsCumulative}}...{{end}}{{if not .Required}}]{{end}}{{end}}\
+{{end}}\
+
+{{define "FormatCommands"}}\
+{{range .FlattenedCommands}}\
+{{if not .Hidden}}\
+  {{.FullCommand}}{{if .Default}}*{{end}}{{template "FormatCommand" .}}
+{{.Help|Wrap 4}}
+{{end}}\
+{{end}}\
+{{end}}\
+
+{{define "FormatUsage"}}\
+{{template "FormatCommand" .}}{{if .Commands}} <command> [<args> ...]{{end}}
+{{if .Help}}
+{{.Help|Wrap 0}}\
+{{end}}\
+
+{{end}}\
+{{if .Context.SelectedCommand}}\
+usage: {{.App.Name}} {{.Context.SelectedCommand}}{{template "FormatUsage" .Context.SelectedCommand}}
+{{else}}\
+usage: {{.App.Name}}{{template "FormatUsage" .App}}
+{{end}}\
+
+Environment variables:
+    KAFKA_BROKERS or TLS_KAFKA_BROKERS
+    SSL_CA_BUNDLE_PATH
+    SSL_CRT_PATH
+    SSL_KEY_PATH
+	
+{{if .Context.Flags|RequiredFlags}}\
+Required flags:
+{{.Context.Flags|RequiredFlags|FlagsToTwoColumns|FormatTwoColumns}}
+{{end}}\
+{{if  .Context.Flags|OptionalFlags}}\
+Optional flags:
+{{.Context.Flags|OptionalFlags|FlagsToTwoColumns|FormatTwoColumns}}
+{{end}}\
+{{if .Context.Args}}\
+Args:
+{{.Context.Args|ArgsToTwoColumns|FormatTwoColumns}}
+{{end}}\
+{{if .Context.SelectedCommand}}\
+Subcommands:
+{{if .Context.SelectedCommand.Commands}}\
+{{template "FormatCommands" .Context.SelectedCommand}}
+{{end}}\
+{{else if .App.Commands}}\
+Commands:
+{{template "FormatCommands" .App}}
+{{end}}\
+`
+
 func main() {
 	app := kingpin.New("k", "A general command line client for Apache Kafka.")
 	app.HelpFlag.Short('h')
 	app.Version("0.1.0")
+	app.UsageTemplate(FlagsUsageTemplate)
 	configureConsumeCommand(app)
 	configureConsumerGroupsCommand(app)
 	configureOffsetsCommand(app)
 	configureProduceCommand(app)
 	configureTLSCommand(app)
 	configureTopicsCommand(app)
+
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 }
 
